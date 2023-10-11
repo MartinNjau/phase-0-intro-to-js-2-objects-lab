89b6e00446cfd5a6b7c3bc61592d4251d24b5fc0
Martin Njau <njau_martin@yahoo.comgit config --global user.email njau_martin@yahoo.comclearclear>
2023-10-11 17:11:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+let employee={name:"John",streetAddress:285};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+let newemp ={...employee};
+newemp={name:"Sam",streetAddress:"11 Broadway"}
+
+return newemp;
+
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    Object.assign(employee, {name:"Sam",streetAddress:"12 Broadway"});
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let updEmp={...employee};
+    delete updEmp.name;
+    return updEmp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee.name;
+    return employee;
+}
